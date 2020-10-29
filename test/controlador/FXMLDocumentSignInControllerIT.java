df34cc6a99e4b2fb5600041d51909b9603842e70
endikau22 <64834124+endikau22@users.noreply.github.com>
2020-10-29 12:54:00 +0100

    Commit 29/10/2020

---
+++ test/controlador/FXMLDocumentSignInControllerIT.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 2dam
+ */
+public class FXMLDocumentSignInControllerIT {
+    
+    public FXMLDocumentSignInControllerIT() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        
+    }
+    
+}
